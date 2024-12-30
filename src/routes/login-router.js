471afa92f48da07c33e192524ed46df27cf976d9
Tamilselvan K <tamils1978@gmail.com>
2024-12-30 17:22:08 +0530

    CORS
    CORS

--- src/routes/login-router.js
+++ src/routes/login-router.js
@@ -4,12 +4,25 @@ const PasswordController = require('../controller/password-controller');
 const {AuthToken} = require('stashook-utils');
 const router = express.Router();
 
-router.post('/login', LoginController.authenticate);
+const cors =require('cors');
+var whitelist = ['http://localhost:4200']
+let corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+router.options('*',cors());
+
+router.post('/login', cors(corsOptionsDelegate), LoginController.authenticate);
 
 router.post('/forgotPassword', PasswordController.forgotPassword);
 
 router.post('/changePassword', AuthToken.validateToken, PasswordController.changePassword);
 
-router.post('/getUser',  AuthToken.validateToken, LoginController.getUser);
+router.post('/getUser',  cors(corsOptionsDelegate), AuthToken.validateToken, LoginController.getUser);
 
 module.exports = router;
