72d4959ebe5548dd08625053405873020765e41e
muthuslm2006 <98971383+muthuslm2006@users.noreply.github.com>
2025-01-23 13:05:39 +0530

    cors implement

--- src/routes/menu-router.js
+++ src/routes/menu-router.js
@@ -4,6 +4,20 @@ const MenuController = require('../controller/menu-controller');
 
 const router = express.Router();
 
-router.post('/menuList', AuthToken.validateToken, MenuController.getMenuList);
+const cors =require('cors');
+var whitelist = ['http://localhost:4200']
+let corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+router.options('*',cors());
+
+
+router.post('/menuList', cors(corsOptionsDelegate),AuthToken.validateToken, MenuController.getMenuList);
 
 module.exports = router;
